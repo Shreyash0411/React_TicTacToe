1cf599f7d19f8a8dc845c9121f936da8d86453b3
Shreyash <shreyash0411srivastava@gmail.com>
2023-05-13 13:16:33 +0530

    shifted all the major logic from board component to app component. Also showed the player turn status and winner declaration to the user

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-
-//import { useState } from 'react';
 import './styles.scss'
+import { useState } from 'react';
 import Board from './Components/board'
+import { calculateWinner } from './calculateWinner';
 
 function App() {
-  return <div className='app'>
-  <Board> </Board>
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+  const winner = calculateWinner(squares); 
+  const nextPlayer = isXNext ? "X" : "O";
+  const statusMessage = winner? `winner is ${winner}` :  `next player is ${nextPlayer}`;
+
+  const handleSquareClicks = clickedPosition=>{
+        if (squares[clickedPosition] || winner){
+            return;
+        }
+    
+        setSquares(currentSquares => {
+            return currentSquares.map((squareValue,pos)=>{
+                if (pos == clickedPosition){
+                        return isXNext? 'X' : 'O';
+                    }
+
+                return squareValue;
+            });
+        });
+        setIsXNext((currentIsNext) => !currentIsNext)
+    };
+
+  return(
+   <div className='app'>
+    <h1>{statusMessage}</h1>
+  <Board squares={squares} handleSquareClicks = {handleSquareClicks}/>
+  
   </div>
+  )
 }
 
 export default App;
--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,34 +1,7 @@
-import { useState } from "react";
-
 import Square from "./Square";
 
-const Board = ()=>
+const Board = ({squares, handleSquareClicks})=>
 {
-    const [squares, setSquares] = useState(Array(9).fill(null));
-
-    const [isXNext, setIsXNext] = useState(false);
-
-    const handleSquareClicks = clickedPosition=>{
-
-        if (squares[clickedPosition])
-        {
-            return;
-        }
-
-        setSquares((currentSquares) => {
-
-            return currentSquares.map((squareValue,pos)=>{
-
-                if (pos == clickedPosition)
-                    {
-                        return isXNext? 'X' : 'O';
-                    }
-
-                return squareValue;
-            });
-        })
-        setIsXNext((currentIsNext) => !currentIsNext)
-    }
 
     const render = position =>{
         return (
@@ -39,7 +12,8 @@ const Board = ()=>
         );
     };
 
-    return <div className="board">
+    return (
+    <div className="board">
     <div className="board-row">
         {render(0)}
         {render(1)}
@@ -54,10 +28,9 @@ const Board = ()=>
         {render(6)}
         {render(7)}
         {render(8)}
+    </div>    
     </div>
-    
-    
-    </div>
+    );
 }
 
 export default Board;
